fbe6e952c28cc2ce136eadfa42bdd0abee682a60
HarshvardhanJosh <harshitjoshi001@gmail.com>
2022-08-14 01:00:12 +0530

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href="/">EShop</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <a href="/">EShop</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Paris Saint-Germain 2022/23 Away Jersey',
+      slug: 'nike-slim-shirt',
+      category: 'shirts',
+      image: '/images/p1.jpg',
+      price: 110,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        'Like other shirts from our Match collection, this one pairs authentic design details with lightweight, quick-drying fabric to help keep the world/s biggest football stars cool and comfortable on the pitch',
+    },
+    {
+      name: 'Nike Dri-FIT',
+      slug: 'nike-dri-fit',
+      category: 'shirts',
+      image: '/images/p2.jpg',
+      price: 50,
+      countInStock: 6,
+      brand: 'Nike',
+      rating: 4.4,
+      numReviews: 18,
+      description:
+        'The Nike Dri-FIT Pullover Training Top gives you the option to chase your dreams or crush your goals.',
+    },
+    {
+      name: 'Giannis',
+      slug: 'nike-slim-pants',
+      category: 'pants',
+      image: '/images/p3.jpg',
+      price: 60,
+      countInStock: 12,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 11,
+      description:
+        'He/s a freak of nature, a champion and power to behold on the court. Celebrate those qualities of Giannis by suiting up in these lightweight trousers',
+    },
+    {
+      name: 'SL Single Jersey to Pants',
+      slug: 'addidas-slim-pants',
+      category: 'pants',
+      image: '/images/p4.jpg',
+      price: 78,
+      countInStock: 10,
+      brand: 'Addidas',
+      rating: 4.3,
+      numReviews: 10,
+      description:
+        'Cotton workout pants that keep you comfortable through your training sessions',
+    },
+  ],
+}
+export default data
